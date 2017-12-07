0280521917937508d87f53959e8fd7d4716ed8af
Jayanth Venkataraman <jayanth.venkataraman@ITIN000030-MAC.local>
2017-12-07 18:06:25 +0530

    Committing changes to return json

--- app/app.py
+++ app/app.py
@@ -2,17 +2,30 @@ __author__ = 'jayanthvenkataraman'
 
 from flask import Flask
 import os
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/login', methods = ['GET'])
 def login_function():
-    return "JWT"
+    result = {"data": "JWT"}
+    response = app.response_class(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route('/users/<jwt>', methods = ['GET'])
 def grab_id_function(jwt):
-    return "1234"
+    result = {"data": "1234"}
+    response = app.response_class(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000)) #for taking Heroku's PORT environment variable
